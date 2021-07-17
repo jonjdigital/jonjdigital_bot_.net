0d97470d030a3f1d70d8db77fc39ca084abb5bfb
Jon James <jon.james@jonjdigital.com>
2021-07-17 20:34:25 +0100

    initiated bot

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
 
 namespace ConsoleApp1
 {
@@ -6,9 +12,38 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
+            MainAsync().GetAwaiter().GetResult();
         }
-        
-        
+
+        static async Task MainAsync()
+        {
+            string token = "NzI5MDY2NDE2ODc0OTc5NDA5.XwDiAw.ktVq4pO_76JIL1no9E8IqUCJ82A";
+            string prefix = "!";
+
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = token,
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                
+                DiscordMessage msg = e.Message;
+                DiscordUser author = e.Message.Author;
+                DiscordMember authorMember = e.Message.Channel.Guild.GetMemberAsync(author.Id).Result;
+
+                if (author.IsBot) return;
+
+                if (msg.Content.ToLower() == "hi")
+                {
+                    await msg.RespondAsync($"<@{author.Id}> Hi!");
+                }
+            };
+            
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
     }
 }
